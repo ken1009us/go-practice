4ffe44a6c1faa2f87f70715a8e7ffa1f1cb7b315
Ken Wu <kenwu1009us@gmail.com>
2023-12-19 17:00:59 -0800

    update to use subtests

--- src/helloworld/hello_test.go
+++ src/helloworld/hello_test.go
@@ -4,11 +4,33 @@ import (
 	"testing"
 )
 
-func TestSayHello(t *testing.T) {
-	want := "Hello, test!"
-	got := SayHello("test")
+// func TestSayHello(t *testing.T) {
+// 	want := "Hello, test!"
+// 	got := SayHello([]string{"test"})
 
-	if want != got {
-		t.Errorf("Wanted %s, got %s", want, got)
+// 	if want != got {
+// 		t.Errorf("Wanted %s, got %s", want, got)
+// 	}
+// }
+
+func TestSayHello(t *testing.T) {
+	subtests := []struct{
+		items []string
+		result string
+	}{
+		{
+			result: "Hello, world!",
+		},
+		{
+			items: []string{"Matt"},
+			result: "Hello, Matt!",
+		},
 	}
+
+	for _, st := range subtests {
+		if s := SayHello(st.items); s != st.result {
+			t.Errorf("Wanted %s, got %s", st.result, s)
+		}
+	}
+
 }
